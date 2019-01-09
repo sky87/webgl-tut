bd2754a1c54e2bc041ee764159ccf83282b02918
Francesco Cielo <skyams@gmail.com>
2019-01-09 14:38:02 +0100

    Colors

--- src/03-colors.ts
+++ src/03-colors.ts
@@ -4,20 +4,34 @@ import {
 } from "./util";
 
 const vertexShaderSrc = `
+precision mediump float;
+
 attribute vec2 position;
+attribute vec3 color;
+
+uniform float time;
+
+varying vec3 vColor;
 
 void main() {
   gl_Position = vec4(position, 0.0, 1.0);
+  vColor = ((cos((time/5.0)*2.0*3.1415) + 1.0)/2.0*.8 + .2) * color;
 }
 `;
 
 const fragmentShaderSrc = `
+precision mediump float;
+
+varying vec3 vColor;
+
 void main() {
-  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  // gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  gl_FragColor = vec4(vColor, 1.0);
 }
 `;
 
-let triangleVertexBuffer: WebGLBuffer;
+let positionBuffer: WebGLBuffer;
+let colorBuffer: WebGLBuffer;
 let shaderProgram: ShaderProgram;
 setupDemo({
   setup(gl: WebGLRenderingContext) {
@@ -26,15 +40,14 @@ setupDemo({
       gl,
       vertexShaderSrc,
       fragmentShaderSrc,
-      ["position"],
-      []
+      ["position", "color"],
+      ["time"]
     );
 
-    // Tell opengl we need a buffer to store the vertices of the triangle
-    triangleVertexBuffer = gl.createBuffer();
+    positionBuffer = gl.createBuffer();
+    colorBuffer = gl.createBuffer();
 
-    // Send the triangle vertices to opengl
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBuffer);
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     gl.bufferData(
       gl.ARRAY_BUFFER,
       new Float32Array([
@@ -48,7 +61,24 @@ setupDemo({
       ]),
       gl.STATIC_DRAW,
     );
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(
+      gl.ARRAY_BUFFER,
+      new Float32Array([
+        1.0, 0.0, 0.0,
+        0.0, 1.0, 0.0,
+        1.0, 1.0, 0.0,
+
+        0.0, 0.0, 1.0,
+        0.0, 1.0, 0.0,
+        0.0, 1.0, 1.0,
+      ]),
+      gl.STATIC_DRAW,
+    );
   },
+
+  raf: true,
 
   draw(gl: WebGLRenderingContext) {
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
@@ -60,13 +90,21 @@ setupDemo({
     // Tell opengl to use the program we compiled previously
     gl.useProgram(shaderProgram.id);
 
-    // Bind the buffer to the position attribute in the vertex shader
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBuffer);
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     gl.vertexAttribPointer(
       shaderProgram.attribute("position"),
       2, gl.FLOAT, false, 0, 0
     );
     gl.enableVertexAttribArray(shaderProgram.attribute("position"));
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.vertexAttribPointer(
+      shaderProgram.attribute("color"),
+      3, gl.FLOAT, false, 0, 0
+    );
+    gl.enableVertexAttribArray(shaderProgram.attribute("color"));
+
+    gl.uniform1f(shaderProgram.uniform("time"), performance.now()/1000);
 
     // Tell opengl to draw triangles
     gl.drawArrays(gl.TRIANGLES, 0, 6);
